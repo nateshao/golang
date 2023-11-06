e4bb2b749dc5b814f8bbb096aeb7bccd6a4397af
shaotj <shaotj@300624.cn>
2023-11-06 21:39:32 +0800

    方法

--- day3_function/code8_gotest/split_test.go
+++ day3_function/code8_gotest/split_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -38,4 +39,86 @@ func TestMoreSplit(t *testing.T) {
 
 func TestChineseSplit(t *testing.T) {
 
+}
+
+/**
+Go提供内置功能来检查你的代码覆盖率。我们可以使用go test -cover来查看测试覆盖率。
+执行go tool cover -html=c.out，使用cover工具来处理生成的记录信息，该命令会打开本地的浏览器窗口生成一个HTML报告。
+*/
+func TestSplitDemo(t *testing.T) {
+	type test struct { // 定义test结构体
+		input string
+		sep   string
+		want  []string
+	}
+	tests := map[string]test{ // 测试用例使用map存储
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong sep":   {input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		"leading sep": {input: "枯藤老树昏鸦", sep: "老", want: []string{"枯藤", "树昏鸦"}},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) { // 使用t.Run()执行子测试
+			got := Split(tc.input, tc.sep)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("excepted:%#v, got:%#v", tc.want, got)
+			}
+		})
+	}
+}
+func BenchmarkSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Split("枯藤老树昏鸦", "老")
+	}
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("write setup code here...") // 测试之前的做一些设置
+	// 如果 TestMain 使用了 flags，这里应该加上flag.Parse()
+	retCode := m.Run()                         // 执行测试
+	fmt.Println("write teardown code here...") // 测试之后做一些拆卸工作
+	os.Exit(retCode)                           // 退出测试
+}
+
+// 测试集的Setup与Teardown
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:测试之后的teardown")
+	}
+}
+
+// 子测试的Setup与Teardown
+func setupSubTest(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:子测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:子测试之后的teardown")
+	}
+}
+
+func TestSplitDemoTwo(t *testing.T) {
+	type test struct { // 定义test结构体
+		input string
+		sep   string
+		want  []string
+	}
+	tests := map[string]test{ // 测试用例使用map存储
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong sep":   {input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		"leading sep": {input: "枯藤老树昏鸦", sep: "老", want: []string{"", "枯藤", "树昏鸦"}},
+	}
+	teardownTestCase := setupTestCase(t) // 测试之前执行setup操作
+	defer teardownTestCase(t)            // 测试之后执行testdoen操作
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) { // 使用t.Run()执行子测试
+			teardownSubTest := setupSubTest(t) // 子测试之前执行setup操作
+			defer teardownSubTest(t)           // 测试之后执行testdoen操作
+			got := Split(tc.input, tc.sep)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("excepted:%#v, got:%#v", tc.want, got)
+			}
+		})
+	}
 }
