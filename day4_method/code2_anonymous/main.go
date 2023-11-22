573ca5cc40539648c2a2ea31cb9cc4a061bae74e
shaotj <shaotj@300624.cn>
2023-11-22 19:36:26 +0800

    @千羽 add命令行演示

--- day4_method/code2_anonymous/main.go
+++ day4_method/code2_anonymous/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+//type User struct {
+//	id   int
+//	name string
+//}
+//
+//type Manager struct {
+//	User
+//}
+//
+//func (self *User) ToString() string { // receiver = &(Manager.User)
+//	return fmt.Sprintf("User: %p, %v", self, self)
+//}
+//
+//func main() {
+//	m := Manager{User{1, "Tom"}}
+//	fmt.Printf("Manager: %p\n", &m)
+//	fmt.Println(m.ToString())
+//}
+
+//
+
+
+
 type User struct {
 	id   int
 	name string
@@ -9,14 +32,21 @@ type User struct {
 
 type Manager struct {
 	User
+	title string
 }
 
-func (self *User) ToString() string { // receiver = &(Manager.User)
+func (self *User) ToString() string {
 	return fmt.Sprintf("User: %p, %v", self, self)
 }
 
+func (self *Manager) ToString() string {
+	return fmt.Sprintf("Manager: %p, %v", self, self)
+}
+
 func main() {
-	m := Manager{User{1, "Tom"}}
-	fmt.Printf("Manager: %p\n", &m)
+	m := Manager{User{1, "Tom"}, "Administrator"}
+
 	fmt.Println(m.ToString())
+
+	fmt.Println(m.User.ToString())
 }
