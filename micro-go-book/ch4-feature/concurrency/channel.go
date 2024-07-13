a59679aa4c89345f54be3df4a52ec17a7c09120a
shaotj <shaotj@300624.cn>
2024-07-13 15:40:08 +0800

    go微服务实战

--- micro-go-book/ch4-feature/concurrency/channel.go
+++ micro-go-book/ch4-feature/concurrency/channel.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"time"
 )
 
@@ -30,23 +28,26 @@ func consume(ch chan int) {
 
 func main() {
 
-	// 创建一个无缓冲的 channel
-	ch := make(chan string)
-	go printInput(ch)
+	// ------------------1
 
-	// 从命令行读取输入
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		val := scanner.Text()
-		ch <- val
-		if val == "EOF" {
-			fmt.Println("End the game!")
-			break
-		}
-	}
-	// 程序最后关闭 ch
-	defer close(ch)
+	//// 创建一个无缓冲的 channel
+	//ch := make(chan string)
+	//go printInput(ch)
+	//
+	//// 从命令行读取输入
+	//scanner := bufio.NewScanner(os.Stdin)
+	//for scanner.Scan() {
+	//	val := scanner.Text()
+	//	ch <- val
+	//	if val == "EOF" {
+	//		fmt.Println("End the game!")
+	//		break
+	//	}
+	//}
+	//// 程序最后关闭 ch
+	//defer close(ch)
 
+	// ------------------2
 	//// 创建一个长度为 2 的 channel
 	//ch := make(chan int, 2)
 	//go consume(ch)
@@ -60,26 +61,28 @@ func main() {
 	//
 	//time.Sleep(time.Second)
 
-	//ch1 := make(chan int)
-	//ch2 := make(chan int)
-	//
-	//go send(ch1, 0)
-	//go send(ch2, 10)
-	//
-	//// 主 goroutine 休眠 1s，保证调度成功
-	//time.Sleep(time.Second)
-	//
-	//for {
-	//	select {
-	//	case val := <- ch1: // 从 ch1 读取数据
-	//		fmt.Printf("get value %d from ch1\n", val)
-	//	case val := <- ch2 : // 从 ch2 读取数据
-	//		fmt.Printf("get value %d from ch2\n", val)
-	//	case <-time.After(2 * time.Second): // 超时设置
-	//		fmt.Println("Time out")
-	//		return
-	//	}
-	//}
+	// ------------------3
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go send(ch1, 0)
+	go send(ch2, 10)
+
+	// 主 goroutine 休眠 1s，保证调度成功
+	time.Sleep(time.Second)
+
+	for {
+		select {
+		case val := <-ch1: // 从 ch1 读取数据
+			fmt.Printf("get value %d from ch1\n", val)
+		case val := <-ch2: // 从 ch2 读取数据
+			fmt.Printf("get value %d from ch2\n", val)
+		case <-time.After(2 * time.Second): // 超时设置
+			fmt.Println("Time out")
+			return
+		}
+	}
 
 }
 
@@ -87,7 +90,6 @@ func send(ch chan int, begin int) {
 
 	for i := begin; i < begin+10; i++ {
 		ch <- i
-
 	}
 
 }
